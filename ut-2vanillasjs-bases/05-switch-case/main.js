f76084f98f28511e85bd941753a98ae4d8b205ca
AlvaroCoo8 <al.alvaro.coo.igeno@iesportada.org>
2024-11-23 14:48:25 +0100

    05-switch-case

---
+++ ut-2vanillasjs-bases/05-switch-case/main.js
@@ -0,0 +1,123 @@
+const DEFAULT_DAY = "Número de día inválido";
+
+/** Crear una función que devuelva los días de la semana en función de un número dado, haciendo uso de switch case
+ * 1 -> Lunes
+ * 2 -> Martes
+ * 3 -> Miércoles
+ * 4 -> Jueves
+ * 5 -> Viernes
+ * 6 -> Sábado
+ * 7 -> Domingo
+ * Cualquier otra opción -> DEFAULT_DAY
+ */
+export const getDayOfWeekSC = (day) => {
+  switch (day) {
+    case 1:
+      return "Lunes";
+    case 2:
+      return "Martes";
+    case 3:
+      return "Miércoles";
+    case 4:
+      return "Jueves";
+    case 5:
+      return "Viernes";
+    case 6:
+      return "Sábado";
+    case 7:
+      return "Domingo";
+    default:
+      return DEFAULT_DAY;
+  }
+};
+
+/** Crear una objeto que devuelva los días de la semana en función de un número dado, haciendo uso de un objeto literal
+ * 1 -> Lunes
+ * 2 -> Martes
+ * 3 -> Miércoles
+ * 4 -> Jueves
+ * 5 -> Viernes
+ * 6 -> Sábado
+ * 7 -> Domingo
+ */
+let dayOfWeek = {
+  1: "Lunes",
+  2: "Martes",
+  3: "Miércoles",
+  4: "Jueves",
+  5: "Viernes",
+  6: "Sábado",
+  7: "Domingo",
+};
+
+/** Crear una función que haciendo uso del objeto literal definido arriba
+ * devuelva los días de la semana en función de un número dado
+ * 1 -> Lunes
+ * 2 -> Martes
+ * 3 -> Miércoles
+ * 4 -> Jueves
+ * 5 -> Viernes
+ * 6 -> Sábado
+ * 7 -> Domingo
+ * Cualquier otra opción -> DEFAULT_DAY
+ */
+export const getDayOfWeekObject = (day) => {
+  switch (true) {
+    case day in dayOfWeek:
+      return dayOfWeek[day];
+    default:
+      return DEFAULT_DAY;
+  }
+};
+
+/************************************************ */
+
+const DEFAULT_OPERATOR_ERROR = "Operator invalid";
+
+/**
+ * Crea un calculadora básica que sume, reste, multiplique y divide. Usando switch case
+ * Operadores validos ("+", "-", "*", "/")
+ * En cualquier otro caso debe devolver DEFAULT_OPERATOR_ERROR
+ */
+export const simpleCalculatorSC = (operator, num1, num2) => {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      return num1 / num2;
+    default:
+      return DEFAULT_OPERATOR_ERROR;
+  }
+};
+
+/**
+ * Crea un objeto con los operadores básicos +, -, *, ,/
+ * Cada propieda del objeto debe realizar la operación correspodiente
+ */
+let calculatorObject = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "*": (a, b) => a * b,
+  "/": (a, b) => a / b,
+};
+
+/**
+ * Crea una función que haga uso de objeto que has creado arriba y que además devuelva DEFAULT_OPERATOR_ERROR
+ * si se introduce cualquier cosa que sea diferente a "+", "-", "*", "/"
+ *
+ */
+export const simpleCalculatorObject = (operator, num1, num2) => {
+  switch (operator) {
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      return calculatorObject[operator](num1, num2);
+    default:
+      return DEFAULT_OPERATOR_ERROR;
+  }
+};

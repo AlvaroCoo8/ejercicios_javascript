e896b7bdab5544517a129e9d73a8b919bfd4c94e
AlvaroCoo8 <al.alvaro.coo.igeno@iesportada.org>
2024-11-24 12:34:12 +0100

    01-destructuring

---
+++ ut-4-vanillajs-avanzado/01-destructuring/main.js
@@ -0,0 +1,97 @@
+const student = ["David", ["HTML", "CSS", "JS", "React"], [98, 85, 90, 95]];
+
+/**
+ * Usando Destructuración de arrays,
+ * debes devolver todas las tecnologías
+ * @returns
+ */
+export const getAllTechnologies = () => {
+  let [, tecnologias] = student;
+  return tecnologias;
+};
+
+/**
+ * Usando Destructuración de arrays,
+ * debes devolver todos los valores
+ * @returns
+ */
+export const getAllValues = () => {
+  let [, , numeros] = student;
+  return numeros;
+};
+
+/**
+ * Usando Destructuración de arrays,
+ * debes devolver solo React
+ * @returns
+ */
+export const getReact = () => {
+  let [, [, , , react]] = student;
+  return react;
+};
+
+/**
+ * Usando Destructuración de arrays,
+ * debes devolver CSS y 85
+ * @returns
+ */
+export const getCSSAnd85 = () => {
+  let [, [, css], [, segundoValor]] = student;
+  return [css, segundoValor];
+};
+
+/*****************************************************
+ * ***************************************************
+ ******************************************************/
+const books = [
+  {
+    title: "La vida del lazarillo de Tormes",
+    author: "Anonymous",
+    published: "1554",
+  },
+  {
+    title: "The NeverEnding Story",
+    author: "Michael Ende",
+    published: "1984",
+  },
+  {
+    title: "The Lord of the Rings",
+    author: "J. R. R. Tolkien",
+    published: "1954",
+  },
+  {
+    title: "Beowulf",
+    author: "Anonymous",
+    published: "900",
+  },
+];
+
+/**
+ * Usando Destructuración de arrays y objetos,
+ * debes devolver el título del primer libro
+ * @returns
+ */
+export const getTitleFirstBook = () => {
+  let [{ title }] = books;
+  return title;
+};
+
+/**
+ * Usando Destructuración de arrays y objetos,
+ * debes devolver el año de publicación del segundo libro
+ * @returns
+ */
+export const getPublishedSecondBook = () => {
+  let [, { published }] = books;
+  return published;
+};
+
+/**
+ * Usando Destructuración de arrays y objetos,
+ * debes devolver el título y el autor del último libro
+ * @returns
+ */
+export const getAuthorsAndTitleLastBook = () => {
+  let [, , , { title, author }] = books;
+  return [title, author];
+};
